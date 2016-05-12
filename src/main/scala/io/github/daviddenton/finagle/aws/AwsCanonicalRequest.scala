3113b549c12c375ad3afb387bbc060440274b2f2
David Denton <denton.david@gmail.com>
2016-05-12 09:23:15 +0100

    daveD - fix canonical request test

--- src/main/scala/io/github/daviddenton/finagle/aws/AwsCanonicalRequest.scala
+++ src/main/scala/io/github/daviddenton/finagle/aws/AwsCanonicalRequest.scala
@@ -1,16 +1,12 @@
 package io.github.daviddenton.finagle.aws
 
-import java.nio.charset.StandardCharsets.UTF_8
+import java.net.URLEncoder
 
-import aws.Hex.encode
 import com.twitter.finagle.http.Request
 import io.github.daviddenton.aws.AwsHmacSha256
-import org.jboss.netty.handler.codec.http.QueryStringDecoder
-
-import scala.collection.JavaConverters._
 
 case class AwsCanonicalRequest(request: Request) {
-  val signedHeaders: String = signedHeaders(request)
+  val signedHeaders = signHeaders(request)
   private val canonical = request.method + "\n" +
     request.path + "\n" +
     canonicalQueryString(request) + "\n" +
@@ -18,19 +14,18 @@ case class AwsCanonicalRequest(request: Request) {
     signedHeaders + "\n" +
     payloadHash(request)
 
-  private def signedHeaders(request: Request) =
+  private def signHeaders(request: Request) =
     request.headerMap.map(header => header._1.toLowerCase()).toSeq.sortBy(identity).mkString(";")
 
   private def canonicalHeaders(request: Request) = request.headerMap.
     map(header => (header._1.toLowerCase(), header._2.replaceAll("\\s+", " ").trim()))
     .map(header => header._1 + ":" + header._2).toSeq.sortBy(identity).mkString("\n")
 
-  private def canonicalQueryString(request: Request) =
-    new QueryStringDecoder(request.uri).getParameters.asScala
-      .toSeq
-      .map(param => encode(param._1.getBytes(UTF_8)) + "=" + encode(param._1.getBytes(UTF_8)))
-      .sortBy(identity)
-      .mkString("&")
+  private def canonicalQueryString(request: Request) = request.params
+    .toSeq
+    .map(param => URLEncoder.encode(param._1, "UTF-8") + "=" + URLEncoder.encode(param._2, "UTF-8"))
+    .sortBy(identity)
+    .mkString("&")
 
   private def payloadHash(request: Request) = AwsHmacSha256.hash(request.contentString.getBytes())
 
--- src/test/scala/io/github/daviddenton/finagle/aws/AwsCanonicalRequestTest.scala
+++ src/test/scala/io/github/daviddenton/finagle/aws/AwsCanonicalRequestTest.scala
@@ -2,6 +2,7 @@ package io.github.daviddenton.finagle.aws
 
 import com.twitter.finagle.http.Method.{Get, Put}
 import com.twitter.finagle.http.Request
+import org.jboss.netty.handler.codec.http.QueryStringEncoder
 import org.scalatest.{FunSpec, ShouldMatchers}
 
 class AwsCanonicalRequestTest extends FunSpec with ShouldMatchers {
@@ -9,27 +10,34 @@ class AwsCanonicalRequestTest extends FunSpec with ShouldMatchers {
   describe("AwsCanonicalRequest") {
 
     it("transform minimal request") {
-      AwsCanonicalRequest(Request(Get, "/test")).toString shouldBe
-        "GET\n" +
-          "/test\n" +
-          "\n" +
-          "content-length:0\n\n" +
-          "content-length\n" +
-          "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      val expected = "GET\n" +
+        "/test\n" +
+        "\n" +
+        "content-length:0\n\n" +
+        "content-length\n" +
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+      val request = Request(Get, "/test")
+      request.contentLength = 0
+
+      AwsCanonicalRequest(request).toString shouldBe expected
     }
 
     it("transform full request") {
-      val request = Request(Put, "/put-path")
+      val uri = new QueryStringEncoder("/put-path")
+      uri.addParam("z-param", "z value")
+      uri.addParam("a-param", "a value")
+      uri.addParam("S-param", "s value")
+
+      val request = Request(Put, uri.toString)
       request.headerMap.add("z-header", "  a   value  ")
       request.headerMap.add("a-header", "  another value  ")
-      request.params + "z-param" -> "z value"
-      request.params + "a-param" -> "a value"
-      request.params + "S-param" -> "s value"
       request.contentString = "hello world"
+      request.contentLength = request.contentString.length
 
       AwsCanonicalRequest(request).toString shouldBe "PUT\n" +
         "/put-path\n" +
-        "S+param=s+value&a+param=a+value&z-param=z+value\n" +
+        "S-param=s+value&a-param=a+value&z-param=z+value\n" +
         "a-header:another value\n" +
         "content-length:11\n" +
         "z-header:a value\n" + "\n" +
